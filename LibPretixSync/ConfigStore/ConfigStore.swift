2709be64c157e854ae692b18ac65d71c50a9616e
Daniel Jilg <winsmith@winsmith.de>
2019-03-13 13:10:27 +0100

    Create ConfigStore protocol

---
+++ LibPretixSync/ConfigStore/ConfigStore.swift
@@ -0,0 +1,14 @@
+//
+//  ConfigStore.swift
+//  PretixScan
+//
+//  Created by Daniel Jilg on 13.03.19.
+//  Copyright © 2019 rami.io. All rights reserved.
+//
+
+import Foundation
+
+/// Holds key-value pairs for configuration.
+protocol ConfigStore {
+    
+}
