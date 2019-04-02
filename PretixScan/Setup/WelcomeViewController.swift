5b069628029545e6a6ee16190faaf65ce511f00a
Daniel Jilg <winsmith@winsmith.de>
2019-04-02 20:16:08 +0200

    Setup Finished View

--- PretixScan/Setup/WelcomeViewController.swift
+++ PretixScan/Setup/WelcomeViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class WelcomeViewController: UIViewController, Configurable {
     var configStore: ConfigStore?
 
-    @IBOutlet weak var checkmarkBackgroundView: UIView!
     @IBOutlet private weak var explanationLabel: UILabel!
     @IBOutlet private weak var checkmarkDetailLabel: UILabel!
     @IBOutlet private weak var continueButton: UIButton!
@@ -24,8 +23,6 @@ class WelcomeViewController: UIViewController, Configurable {
         explanationLabel.text = Localization.WelcomeViewController.Explanation
         checkmarkDetailLabel.text = Localization.WelcomeViewController.CheckMarkDetail
         continueButton.setTitle(Localization.WelcomeViewController.Continue, for: .normal)
-        checkmarkBackgroundView.backgroundColor = Color.grayBackground
-        checkmarkBackgroundView.layer.cornerRadius = 10
     }
 
     // MARK: - Actions
---
+++ PretixScan/UIView Subclasses/GroupContainerView.swift
@@ -0,0 +1,26 @@
+//
+//  GroupContainerView.swift
+//  PretixScan
+//
+//  Created by Daniel Jilg on 02.04.19.
+//  Copyright © 2019 rami.io. All rights reserved.
+//
+
+import UIKit
+
+class GroupContainerView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+        backgroundColor = Color.grayBackground
+        layer.cornerRadius = 10
+    }
+}
