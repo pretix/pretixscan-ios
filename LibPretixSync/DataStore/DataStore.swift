b43e952e7a1aa020bf6d619fc7ea0725aa3b14b6
Daniel Jilg <winsmith@winsmith.de>
2019-04-08 17:11:50 +0200

    Create DataStore and SyncManager Protocols

---
+++ LibPretixSync/DataStore/DataStore.swift
@@ -0,0 +1,13 @@
+//
+//  DataStore.swift
+//  PretixScan
+//
+//  Created by Daniel Jilg on 08.04.19.
+//  Copyright © 2019 rami.io. All rights reserved.
+//
+
+import Foundation
+
+protocol DataStore: class {
+
+}
