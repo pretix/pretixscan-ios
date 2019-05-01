422e318e51a6395e4ed1ad8e1483a1515bc3a365
Daniel Jilg <winsmith@winsmith.de>
2019-05-01 20:32:18 +0200

    Calculate correct amounds when logging sync status

--- PretixScan/NotifcationManager.swift
+++ PretixScan/NotifcationManager.swift
@@ -59,14 +59,24 @@ class NotificationManager {
         }
     }
 
+    var previouslyLoadedAmounts = [String: Int]()
+
     @objc
     func syncDownloadStatusUpdate(_ notification: Notification) {
         let model: String = notification.userInfo?[SyncManager.NotificationKeys.model] as? String ?? "No Model"
-        let loadedAmount = notification.userInfo?[SyncManager.NotificationKeys.loadedAmount] as? Int ?? -1
-        let totalAmount = notification.userInfo?[SyncManager.NotificationKeys.totalAmount] as? Int ?? -1
+        let loadedAmount = notification.userInfo?[SyncManager.NotificationKeys.loadedAmount] as? Int ?? 0
+        let totalAmount = notification.userInfo?[SyncManager.NotificationKeys.totalAmount] as? Int ?? 0
         let isLastPage = notification.userInfo?[SyncManager.NotificationKeys.isLastPage] as? Bool ?? false
+        let previouslyLoadedAmount = previouslyLoadedAmounts[model, default: 0]
 
-        print("\(model) updated, added \(loadedAmount)/\(totalAmount).")
+        if isLastPage {
+            // reset load counter
+            previouslyLoadedAmounts[model] = nil
+        } else {
+            previouslyLoadedAmounts[model] = previouslyLoadedAmount + loadedAmount
+        }
+
+        print("\(model) updated, added \(previouslyLoadedAmount + loadedAmount)/\(totalAmount).")
 
         if isLastPage {
             print("Finished syncing \(model).")
