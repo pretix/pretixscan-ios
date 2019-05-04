cd9335c1cec75d1e9518181fce3be51c38dc3613
Daniel Jilg <winsmith@winsmith.de>
2019-05-04 15:04:16 +0200

    Create Prototype APIClientOperation

--- LibPretixSync/SyncManager/SyncOperations.swift
+++ LibPretixSync/SyncManager/SyncOperations.swift
@@ -7,3 +7,109 @@
 //
 
 import Foundation
+
+class APIClientOperation: Operation {
+    // MARK: - Public Properties
+    let apiClient: APIClient
+    let event: Event
+    let checkInList: CheckInList
+
+    // MARK: - Private Properties
+    private var urSessionTask: URLSessionTask?
+
+    // MARK: - KVO Property Management
+    private var _executing = false
+    private var _finished = false
+
+    override var isAsynchronous: Bool { return true }
+
+    override var isExecuting: Bool {
+        get {
+            return _executing
+        } set {
+            willChangeValue(forKey: "isExecuting")
+            _executing = newValue
+            didChangeValue(forKey: "isExecuting")
+        }
+    }
+
+    override var isFinished: Bool {
+        get {
+            return _finished
+        } set {
+            willChangeValue(forKey: "isFinished")
+            _finished = newValue
+            didChangeValue(forKey: "isFinished")
+        }
+    }
+
+    init(apiClient: APIClient, event: Event, checkInList: CheckInList) {
+        self.apiClient = apiClient
+        self.event = event
+        self.checkInList = checkInList
+    }
+
+    // MARK: - Management Methods
+    override func start() {
+        if isCancelled {
+            return
+        }
+
+        isExecuting = true
+
+        // TODO: Do actual work
+        completeOperation()
+    }
+
+    func completeOperation() {
+        isFinished = true
+        isExecuting = false
+    }
+
+    override func cancel() {
+        super.cancel()
+        if isExecuting {
+            isFinished = true
+            isExecuting = false
+        }
+
+        urSessionTask?.cancel()
+    }
+}
+
+class DownloadAllOrders: APIClientOperation {
+    override func start() {
+        if isCancelled {
+            return
+        }
+
+        isExecuting = true
+
+        let task = apiClient.getTask(Order.self, lastUpdated: nil, isFirstGet: true) { result in
+            guard let pagedList = try? result.get() else {
+                // TODO: completionHandler(APIError.emptyResponse)
+                // TODO: Report error somewhere
+                self.completeOperation()
+                return
+            }
+
+            // Notify Listeners
+            let isLastPage = pagedList.next == nil
+            //NotificationCenter.default.post(name: SyncManager.syncStatusUpdateNotification, object: self, userInfo: [
+            //    NotificationKeys.model: model.humanReadableName,
+            //    NotificationKeys.loadedAmount: pagedList.results.count,
+            //    NotificationKeys.totalAmount: pagedList.count,
+            //    NotificationKeys.isLastPage: isLastPage])
+
+            // Store Data
+            // TODO: self.configStore.dataStore?.store(pagedList.results, for: event)
+
+            // Callback that we are completely finished
+            if isLastPage {
+                // TODO: self.configStore.dataStore?.setLastSyncTime(pagedList.generatedAt ?? "", of: model, in: event)
+                self.completeOperation()
+            }
+        }
+        task?.resume()
+    }
+}
