a7e5ab83460c848b8ba5c7a874e4716f57699236
Daniel Jilg <winsmith@winsmith.de>
2019-05-09 15:56:30 +0200

    Do not flash the view when scanning
    Closes #7

--- PretixScan/UIView Subclasses/ScannerViewController.swift
+++ PretixScan/UIView Subclasses/ScannerViewController.swift
@@ -112,7 +112,7 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
         guard let stringValue = readableObject.stringValue else { return }
 
         vibrate()
-        flashView()
+        // flashView()
         found(code: stringValue)
     }
 
