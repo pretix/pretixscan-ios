c48f1dbf7b86ab07da2798e249e02b248dadf075
Daniel Jilg <winsmith@winsmith.de>
2019-05-10 12:25:25 +0200

    Subevent model

---
+++ LibPretixSync/Models/SubEvent.swift
@@ -0,0 +1,79 @@
+//
+//  SubEvent.swift
+//  pretixSCAN
+//
+//  Created by Daniel Jilg on 10.05.19.
+//  Copyright © 2019 rami.io. All rights reserved.
+//
+
+import Foundation
+
+
+/// `Event`s can represent whole event series
+///
+/// if the has_subevents property of the event is active. In this case,
+/// many other resources are additionally connected to an event date
+/// (also called sub-event).
+public struct SubEvent: Model {
+    public static let humanReadableName = "Sub Event"
+    public static let urlPathPart = "subevents"
+
+    /// Internal ID of the sub-event
+    public let identifier: Identifier
+
+    /// The sub-event’s full name
+    public let name: MultiLingualString
+
+    /// The slug of the parent event
+    public let event: String
+
+    /// If true, the sub-event ticket shop is publicly available.
+    public let isActive: Bool
+
+    /// If true, the sub-event ticket shop is publicly shown in lists.
+    public let isPublic: Bool
+
+    /// The sub-event’s start date
+    public let dateFrom: Date
+
+    /// The sub-event’s end date
+    public let dateTo: Date?
+
+    /// The sub-event’s admission date
+    public let dateAdmission: Date?
+
+    /// The sub-date at which the ticket shop opens
+    public let presaleStart: Date?
+
+    /// The sub-date at which the ticket shop closes
+    public let presaleEnd: Date?
+
+    /// The sub-event location
+    public let location: MultiLingualString?
+
+    private enum CodingKeys: String, CodingKey {
+        case identifier
+        case name
+        case event
+        case isActive = "active"
+        case isPublic = "is_public"
+        case dateFrom = "date_from"
+        case dateTo = "date_to"
+        case dateAdmission = "date_admission"
+        case presaleStart = "presale_start"
+        case presaleEnd = "presale_end"
+        case location
+    }
+}
+
+extension SubEvent: Equatable {
+    public static func == (lhs: SubEvent, rhs: SubEvent) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+}
+
+extension SubEvent: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self.identifier)
+    }
+}
