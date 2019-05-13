b0d6503fa16a7c19211b62466dda4039c08aa035
Daniel Jilg <winsmith@winsmith.de>
2019-05-13 18:07:15 +0200

    Enable FMDBDataStore as DataStore

--- LibPretixSync/DataStore/FMDBDataStore.swift
+++ LibPretixSync/DataStore/FMDBDataStore.swift
@@ -11,18 +11,57 @@ import Foundation
 /// DataStore that uses FMDB to store data inside a MySQL file
 ///
 /// - Note: See `DataStore` for function level documentation.
-public class FMDBDataStore {
-
-    private var lastSynced = [String: String]()
-    public func storeLastSynced(_ data: [String: String]) {
-        lastSynced = data
+public class FMDBDataStore: DataStore {
+    public func invalidateLastSynced(in event: Event) {
+        // TODO
     }
 
-    public func retrieveLastSynced() -> [String: String] {
-        return lastSynced
+    public func setLastSyncTime<T>(_ dateString: String, of model: T.Type, in event: Event) where T: Model {
+        // TODO
     }
 
-    public func store<T: Model>(_ resources: [T], for event: Event) {
-
+    public func lastSyncTime<T>(of model: T.Type, in event: Event) -> String? where T: Model {
+        // TODO
+        return nil
     }
+
+    public func store<T>(_ resources: [T], for event: Event) where T: Model {
+        // TODO
+    }
+
+    public func getEvents() -> [Event] {
+        // TODO
+        return []
+    }
+
+    public func getCheckInLists(for event: Event) -> [CheckInList] {
+        // TODO
+        return []
+    }
+
+    public func searchOrderPositions(_ query: String, in event: Event) -> [OrderPosition] {
+        // TODO
+        return []
+    }
+
+    public func redeem(secret: String, force: Bool, ignoreUnpaid: Bool, in event: Event, in checkInList: CheckInList)
+        -> RedemptionResponse? {
+        // TODO
+        return nil
+    }
+
+    public func numberOfRedemptionRequestsInQueue(in event: Event) -> Int {
+        // TODO
+        return 0
+    }
+
+    public func getRedemptionRequest(in event: Event) -> QueuedRedemptionRequest? {
+        // TODO
+        return nil
+    }
+
+    public func delete(_ queuedRedemptionRequest: QueuedRedemptionRequest, in event: Event) {
+        // TODO
+    }
+
 }
