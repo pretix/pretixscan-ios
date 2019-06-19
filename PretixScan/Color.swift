02e3c9d23d0fa8f2082683e325c928d3443172c8
Raphael Michel <michel@rami.io>
2019-06-19 12:41:01 +0200

    Add app icon and color scheme

--- PretixScan/Color.swift
+++ PretixScan/Color.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 struct Color {
-    static let primary = #colorLiteral(red: 0.2332136035, green: 0.1112082228, blue: 0.2892545462, alpha: 1)
+    static let primary = #colorLiteral(red: 0.2312644422, green: 0.1089703217, blue: 0.2912759185, alpha: 1)
     static let primaryText = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    static let secondary = #colorLiteral(red: 0.4965984821, green: 0.354232192, blue: 0.57041049, alpha: 1)
+    static let secondary = #colorLiteral(red: 0.4978353977, green: 0.3521931767, blue: 0.567596674, alpha: 1)
     static let grayBackground = #colorLiteral(red: 0.972464025, green: 0.9726033807, blue: 0.9724336267, alpha: 1)
     static let whiteBackground = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    static let error = #colorLiteral(red: 0.9426549077, green: 0.397357285, blue: 0.2020264566, alpha: 1)
-    static let warning = #colorLiteral(red: 0.9480441213, green: 0.8097700477, blue: 0.2334584594, alpha: 1)
-    static let okay = #colorLiteral(red: 0.6854265928, green: 0.7983864546, blue: 0.0941010341, alpha: 1)
+    static let error = #colorLiteral(red: 0.8258044124, green: 0.3749413788, blue: 0.3758454323, alpha: 1)
+    static let warning = #colorLiteral(red: 0.9982002378, green: 0.7056498528, blue: 0.1012035981, alpha: 1)
+    static let okay = #colorLiteral(red: 0.3154402673, green: 0.6320772767, blue: 0.4040964842, alpha: 1)
 }
 
 struct Style {
