e59eb711131f7fff1e60c3d14abe06bd6a5c1259
Daniel Jilg <winsmith@winsmith.de>
2019-07-01 09:26:23 +0200

    Delete whole Database instead of dropping tables

--- LibPretixSync/DataStore/FMDBDataStore/FMDBDataStore.swift
+++ LibPretixSync/DataStore/FMDBDataStore/FMDBDataStore.swift
@@ -18,7 +18,7 @@ public class FMDBDataStore: DataStore {
     // MARK: Metadata
     /// Remove all Sync Times and pretend nothing was ever synced
     public func invalidateLastSynced(in event: Event) {
-        dropAllTables(with: event)
+        deleteDatabase(for: event)
 
         // Recreate the database
         _ = databaseQueue(with: event, recreate: true)
@@ -347,24 +347,12 @@ public class FMDBDataStore: DataStore {
 }
 
 private extension FMDBDataStore {
-    /// Drop and recreate all tables to thoroughly clean the db
-    /// This includes the SyncTimestamp table
-    func dropAllTables(with event: Event) {
-        databaseQueue(with: event).inDatabase { database in
-            do {
-                try database.executeUpdate(ItemCategory.destructionQuery, values: nil)
-                try database.executeUpdate(Item.destructionQuery, values: nil)
-                try database.executeUpdate(SubEvent.destructionQuery, values: nil)
-                try database.executeUpdate(Order.destructionQuery, values: nil)
-                try database.executeUpdate(OrderPosition.destructionQuery, values: nil)
-                try database.executeUpdate(CheckIn.destructionQuery, values: nil)
-                try database.executeUpdate(QueuedRedemptionRequest.destructionQuery, values: nil)
-                try database.executeUpdate(SyncTimeStamp.destructionQuery, values: nil)
-            } catch {
-                EventLogger.log(event: "db init failed: \(error.localizedDescription)",
-                    category: .database, level: .fatal, type: .error)
-            }
-        }
+    /// Delete the database file
+    func deleteDatabase(for event: Event) {
+        let fileURL = try! FileManager.default
+            .url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            .appendingPathComponent("\(event.slug).sqlite")
+        try! FileManager.default.removeItem(at: fileURL)
     }
 
     func databaseQueue(with event: Event, recreate: Bool = false) -> FMDatabaseQueue {
